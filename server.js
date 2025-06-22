32898837d3989708223c56bf8fedc01198e31e09
unknown <raminrzayev1990@gmail.com>
2025-06-22 08:00:47 +0400

    fixing

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const codes = {};
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "projecttravel1990@gmail.com",
+    pass: "cchs cakp qbmy wgxv"
   },
 });
 
